92c18705b8bfc931c1834faa7048c420bed8e8d3
Chau <ctch5@mail.umsl.edu>
2018-03-04 10:19:08 -0600

    another another blah

--- src/app/services/socket-client.service.ts
+++ src/app/services/socket-client.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
-import { last } from 'rxjs/operators';
 import * as io from 'socket.io-client';
 
 @Injectable()
@@ -10,7 +9,7 @@ export class SocketClientService {
     private socket: SocketIOClient.Socket;
     private rideId: string;
     private rideIdSub: BehaviorSubject<string> = new BehaviorSubject<string>('');
-    rideIdObs = this.rideIdSub.asObservable().pipe(last());
+    rideIdObs = this.rideIdSub.asObservable();
 
     constructor() {
         this.socket = io.connect('https://server-91lyft.herokuapp.com/');
@@ -25,6 +24,7 @@ export class SocketClientService {
     }
 
     onRideStatusUpdated(rideId: string) {
+        console.log(rideId);
         return new Observable(observer => {
             this.socket.on(`ride.status.updated_${rideId}`, data => {
                 observer.next(data);
